90e0e3350e393f0b157d93f901d2e4b253416bc6
kimayasarin15 <sarik831@newschool.edu>
2026-02-19 16:51:08 -0500

    up and down buttons

--- script.js
+++ script.js
@@ -67,10 +67,10 @@ toggleButton.addEventListener('click', () => {
 // })
 
 
-// this is my modal js for my header button - I re wrote it because the one below was only looking in channel blocks and this was in my header but will look into seeing if theres a way to combine them
+// this is my modal js for my main button - I re wrote it because the one below was only looking in channel blocks and this isn't in CB but will look into seeing if theres a way to combine them
 
 let modalButton = document.querySelector('.modal-button')
-let modalDialog = document.querySelector('header dialog')
+let modalDialog = document.querySelector('main dialog')
 let closeButton = modalDialog.querySelector('.close-button')
 
 modalButton.addEventListener('click', () => {
